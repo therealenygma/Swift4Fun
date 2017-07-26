708999e5a9918d82baa93090cbb2c8f934617c50
Todd Clark <toddclark@a0mkgq17.engineering.iastate.edu>
2017-07-26 14:01:00 -0500

    Initial Commit

---
+++ Swift 4 Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift 4 Fun
+//
+//  Created by Todd Clark on 7/26/17.
+//  Copyright © 2017 Todd Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
